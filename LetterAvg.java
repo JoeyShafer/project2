bd554bf2d0d9049d2198f0031e07869fed04b6f9
Joey Shafer <Joseph.m.shafer-1@ou.edu>
2019-10-06 22:45:57 -0500

    Added read mehtod

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,10 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class LetterAvg {
 	
+	private ArrayList<String> stations = new ArrayList<String>();
 	private char letter;
 	
-	public LetterAvg (char letter) {
+	public LetterAvg (char letter) throws IOException {
+		read();
 		this.letter = letter;
+	}
+	
+	/*
+	 * Loads a list of station Id's from a txt file. Each line repersents one station.
+	 * 
+	 *  @Param filename name of txt file that will be read in.
+	 */
+	public void read() throws IOException
+	{
+		// Declare a BufferReader to read in file.
+		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
+       String strg;
+           
+       // Read in and throw out the first five lines of the file to get to station Id's.
+       for (int i = 0; i < 6; ++i) {
+       	strg = br.readLine();
+     
+       }
+       
+       //read in first line of station Id's.
+       strg = br.readLine();
+       
+
+       while (strg != null) {
+       
+       	
+       	// Get the station Id from the line and add to staion array.
+       	String stationID = strg.substring(4, 8);
+       	stations.add(stationID);
+       	
+       	//Read next line and increase lineNum and index.
+       	strg = br.readLine();
+       	
+       }
+      
+       br.close();
+		
 	}
 
 }
