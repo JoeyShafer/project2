df26587dace0ffccda04fed7291988429a1e7de4
Joey Shafer <Joseph.m.shafer-1@ou.edu>
2019-10-08 00:39:26 -0500

    Added toString method to PosAvg

--- Driver.java
+++ Driver.java
@@ -8,15 +8,15 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException 
 	{
-		/*
-		String stID = "OKCE";
-		MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
+		
+		String stID = "NRMN";
+		//MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
 		
 		PosAvg posAvg = new PosAvg(stID);
 		System.out.println("The Index of the city is: " + posAvg.indexOfStation());
 		System.out.println(posAvg);
 		System.out.print("\n");
-		
+		/*
 		System.out.println("Ascii Ceiling is " + mesoInherit.calAverage()[0]);
 		System.out.println("Ascii Floor is " + mesoInherit.calAverage()[1]);
 		System.out.println("Ascii Average is " + mesoInherit.calAverage()[2]);
--- PosAvg.java
+++ PosAvg.java
@@ -65,17 +65,41 @@ public class PosAvg  {
 	 * 
 	 * @Param stID the id of the station
 	 */
-	public int indexOfStation(String stID) {
+	public int indexOfStation() {
 		int indexOfStation = 0;
 		
 		//Loops through stations array to find the stID and gets the index of it.
 		for (int i = 0; i < stations.size(); ++i) {
 			if (stID.equalsIgnoreCase(stations.get(i))) {
-				indexOfStation = i;
+				indexOfStation = i + 1;
 				break;
 			}
 		}
 		return indexOfStation;
+	}
+	
+	/*
+	 * Uses the index of a station to find stations whoese average is that index and adds 
+	 * their id to an array of strings
+	 * 
+	 * @Param index index of a sation in the stations array
+	 */
+	public String[] indexAvgOfStations(int index) {
+		String[] avgStations = new String[4];
+		avgStations[0] = stations.get(index - 2);
+		avgStations[1] = stations.get(index);
+		avgStations[2] = stations.get(index - 3);
+		avgStations[3] = stations.get(index + 1);
+		return avgStations;
+	}
+	
+	@Override
+	public String toString() {
+		int index  = indexOfStation();
+		String[] avgStations = indexAvgOfStations(index);
+		return String.format("This index is average of %s and %s, and %s and %s, and so on."
+				, avgStations[0], avgStations[1], avgStations[2], avgStations[3]);
+	
 	}
 	
 }
