03542708ed46b654d1184a6533e35f23aa334123
Joey Shafer <Joseph.m.shafer-1@ou.edu>
2019-10-08 16:24:59 -0500

    added comments

--- MesoInherit.java
+++ MesoInherit.java
@@ -15,17 +15,31 @@ public class MesoInherit  extends MesoAbstract {
 		this.stID = stID;
 	}
 	
+	/*
+	 * Converts the station Id to ascii numbers and finds the average of them 
+	 * then finds the ceiling, floor and average of the number and stores them in an array
+	 * of ints.
+	 * 
+	 * @see MesoAbstract#calAverage()
+	 */
 	public int[] calAverage() {
+		//Create array to store the average and the station id in ascii form
 		int[] calAverage = new int[3];
 		int[] ascii = new int[stID.length()];
+		
+		//converts station id to ascii numbers
 		for (int i = 0; i < stID.length(); ++i) {
 			ascii[i] = (int)stID.charAt(i);
 		}
+		
+		//finds the average of the ascii numbers
 		double average = 0;
 		for (int i = 0; i < ascii.length; ++i) {
 			average += ascii[i];
 		}
 		average = average/ascii.length;
+		
+		//Finds the ceiling, floor, as well as rounds the average up or down 
 		calAverage[0] = (int)Math.ceil(average);
 		calAverage[1] = (int)Math.floor(average);
 		calAverage[2] = (int)Math.round(average);
@@ -33,7 +47,13 @@ public class MesoInherit  extends MesoAbstract {
 		return calAverage;
 	}
 	
+	/*
+	 * Gets the letter that is repersented by the average ascii number of a station id.
+	 * 
+	 * @see MesoAbstract#letterAverage()
+	 */
 	public char letterAverage() {
+		//converts average ascii number to a char that repersents a letter.
 		char letter = (char)calAverage[2];
 		return letter;
 		
